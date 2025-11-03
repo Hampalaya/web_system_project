97fa6c28b2e6a7534110c0d806ed727a83e393c2
Neil Delmo <jmardelmo@gmail.com>
2025-11-03 20:16:02 +0800

    edited again

--- config/database.php
+++ config/database.php
@@ -58,12 +58,10 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql')
-                ? array_filter(env('APP_ENV') === 'local' || env('APP_ENV') === 'testing' ? [] : [
-                    PDO::MYSQL_ATTR_SSL_CA => base_path('DigiCertGlobalRootCA.crt.pem'),
-                    PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => true,
-                ])
-                : [],
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+              
         ],
 
         'mariadb' => [
